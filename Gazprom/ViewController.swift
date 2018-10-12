8f52ae1a818eb8c49105175fd90628dea08a16b3
Tennet <tennet0505@gmail.com>
2018-10-12 13:58:08 +0600

    Initial Commit

---
+++ Gazprom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gazprom
+//
+//  Created by Oleg Ten on 10/12/18.
+//  Copyright © 2018 Oleg Ten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
