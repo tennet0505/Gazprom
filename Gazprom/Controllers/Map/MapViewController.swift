ca8597d5fd68bf5aab966328f02ecb5825725f0e
Tennet <tennet0505@gmail.com>
2018-10-15 11:23:03 +0600

    add own location

--- Gazprom/Controllers/Map/MapViewController.swift
+++ Gazprom/Controllers/Map/MapViewController.swift
@@ -8,19 +8,97 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
-    @IBOutlet weak var mapKit: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var adressLabel: UILabel!
     
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        checkLocationServices()
+        
+    }
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthtorization()
+        }else{
+            //show alert
+        }
+    }
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    func checkLocationAuthtorization()  {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            //show alert turn on permission
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //show alert 
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    
+    func getCenterLocation(for MapView: MKMapView) -> CLLocation {
+        let latitude = MapView.centerCoordinate.latitude
+        let longitude = MapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
         
     }
     
-
+}
+extension MapViewController: CLLocationManagerDelegate{
     
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D.init(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthtorization()
+    }
+}
+extension MapViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            if  let _ = error{
+                return
+            }
+            guard let placemark = placemarks?.first else{
+                return
+            }
+        }
+    }
 }
