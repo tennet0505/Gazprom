6d34ac313957d21fd6a74ee57c8173120fa0a164
Tennet <tennet0505@gmail.com>
2018-10-17 18:01:14 +0600

    add alerts

--- Gazprom/Controllers/PersonalAccounts/NewBillViewController.swift
+++ Gazprom/Controllers/PersonalAccounts/NewBillViewController.swift
@@ -25,9 +25,36 @@ class NewBillViewController: UIViewController, UITextFieldDelegate {
     }
     @IBAction func payButtonOnView(_ sender: Any) {
         
-       navigationController?.popViewController(animated: false)
+        if addPayTextfield.text == ""{
+            showAlert()
+        }else{
+            alertToPayment(lsString:  acoountLabelonView.text ?? "NoAccounts" , numbers: addPayTextfield.text ?? "NoNumbers" )
+        }
         
     }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Введите показания счетчика!!!", message: "" , preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func alertToPayment(lsString: String, numbers: String){
+        let alert = UIAlertController(title: "Лицевой счет: \(lsString)", message: "Показания счетчика: \(numbers)" , preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .cancel) { (alert) in
+             self.navigationController?.popViewController(animated: false)
+        }
+        let action2 = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        alert.addAction(action2)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
